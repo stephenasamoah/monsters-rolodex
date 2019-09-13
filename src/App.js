0096a8e17da2dcd778d77af18d104462bd333851
Stephen Asamoah <stephenasamoah@users.noreply.github.com>
2019-09-13 08:34:16 +0000

    Moved react render function into a class Component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import logo from './logo.svg'
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={ logo } className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
-    );
+class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            displayText: 'Hello, Asamoah'
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={ logo } className="App-logo" alt="logo"/>
+                    <p>
+                        { this.state.displayText }
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+
+                    <button onClick={ () => this.setState({ displayText: 'Welcome' }) }>Change text</button>
+                </header>
+            </div>
+        )
+    }
+
+    swapState() {
+        return this.setState({ displayText: 'Welcome' })
+    }
 }
+
+// function App() {
+//   return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={ logo } className="App-logo" alt="logo"/>
+//           <p>
+//             Edit <code>src/App.js</code> and save to reload.
+//           </p>
+//           <a
+//               className="App-link"
+//               href="https://reactjs.org"
+//               target="_blank"
+//               rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//   );
+// }
 
 export default App;
