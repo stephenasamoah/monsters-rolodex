a572ea3371b4a33f914492d10623544d2fb6eb8c
Stephen Asamoah <stephenasamoah@users.noreply.github.com>
2020-04-12 23:50:26 +0000

    Updates

---
+++ precache-manifest.fc995cb131f5f431527b124119f7a089.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fc607899ca29644d5ed61acf9d389b2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8fb4ca0ff577aff6293e",
+    "url": "/monsters-rolodex/static/css/main.b259ea90.chunk.css"
+  },
+  {
+    "revision": "e57ff400467a2f25bbe9",
+    "url": "/monsters-rolodex/static/js/2.1a8f040e.chunk.js"
+  },
+  {
+    "revision": "8fb4ca0ff577aff6293e",
+    "url": "/monsters-rolodex/static/js/main.1eec725e.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
